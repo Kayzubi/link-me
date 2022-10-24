a9d81dd7c2bc25a7fa798e015e88aceb40721f55
Kizito Azubuike <kayzubi84@gmail.com>
2022-10-24 23:07:19 +0100

    set up page routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound'
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Hello Word</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <header className='App-header'>
+          <h1>Hello Word</h1>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
