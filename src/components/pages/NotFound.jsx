db8be0645582a1a654e6566f91a77aa91d60b2c1
Kizito Azubuike <kayzubi84@gmail.com>
2022-10-26 19:01:18 +0100

    complete error page

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,5 +1,31 @@
+import { useNavigate, Link } from 'react-router-dom'
+import { IoIosArrowRoundBack } from 'react-icons/io'
+import './NotFound.css'
+
 function NotFound() {
-  return <div>NotFound</div>
+  let navigate = useNavigate()
+
+  return (
+    <div className='error'>
+      <div className='container'>
+        <p className='error-type'>404 error</p>
+        <h1 className='error-heading'>We can't find that page</h1>
+        <p className='error-message'>
+          Sorry, the page you are looking for doesn't exist or has been moved.
+        </p>
+        <div className='error-buttons'>
+          <button
+            className='error-btn error-btn-outline'
+            onClick={() => navigate(-1)}>
+            <IoIosArrowRoundBack /> Go back
+          </button>
+          <Link className='error-btn error-btn-primary' to={'/'}>
+            Take me home
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NotFound
