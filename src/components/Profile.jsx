4aa6204e86986a9efc1d8bb6a57f275aef6c1f81
Kizito Azubuike <kayzubi84@gmail.com>
2022-10-28 11:00:00 +0100

    update share icon

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import Image from './assets/IMG_9036.jpeg'
 import { AiOutlineCamera } from 'react-icons/ai'
 import { ReactComponent as Share } from './assets/sharebutton.svg'
+import { ReactComponent as More } from './assets/more.svg'
 import Tooltip from './Tooltip'
 import './Profile.css'
 
@@ -8,11 +9,15 @@ function Profile() {
   return (
     <div className='profile'>
       <Tooltip
-        tooltip={<Share />}
+        tooltip={
+          <>
+            <Share className='share' /> <More className='more' />
+          </>
+        }
         tooltipText={'Share link'}
         direction={'left'}
       />
-      <div className='profile-img'>
+      <div className='profile-img-container'>
         <img src={Image} alt='Profile-img' id='profile_img' />
         <AiOutlineCamera color='#ffffff' className='camera' />
       </div>
